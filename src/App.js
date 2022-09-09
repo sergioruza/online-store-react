714ab50441dcf8a56a119abda14563ca5ab3cddd
Állex <allexwithx@gmail.com>
2022-09-09 15:32:11 -0300

    Requisito 03

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ProductListing from './pages/ProductListing';
+import Cart from './pages/Cart';
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ ProductListing } />
+          <Route exact path="/cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     );
