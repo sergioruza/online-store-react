cb381fc20e65e8ec76287a9a3d81d17bb99b475e
HiagoP <hiagoperonip@gmail.com>
2022-09-13 16:11:04 -0300

    'Requisito 10 pronto correção :rocket:'

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,8 +4,6 @@ import ItensCarrinho from '../components/ItensCarrinho';
 class Cart extends React.Component {
   state = {
     data: [],
-    // disabled: false,
-    quantidade: 1,
   };
 
   componentDidMount() {
@@ -15,26 +13,6 @@ class Cart extends React.Component {
   getStorage = () => {
     const data = JSON.parse(localStorage.getItem('produtos'));
     if (data) this.setState({ data });
-  };
-
-  // aumentarProduto = ({ target }) => {
-  //   // const soma = Number(target.parentNode.previousSibling.innerText) + 1;
-  //   // target.parentNode.previousSibling.innerText = soma;
-  //   // this.setState({ quantidade: soma });
-  //   // this.setState({ disabled: false });
-  // };
-
-  aumentarProduto = () => {
-    this.setState((previousState) => ({ quantidade: previousState.quantidade + 1 }));
-  };
-
-  diminuirProduto = ({ target }) => {
-    const sub = Number(target.parentNode.previousSibling.innerText) - 1;
-    const minNumber = 2;
-    if (Number(target.parentNode.previousSibling.innerText) < minNumber) {
-      return true;
-    }
-    (target.parentNode.previousSibling.innerText = sub);
   };
 
   render() {
