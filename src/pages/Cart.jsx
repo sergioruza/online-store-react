fd0b2669f2b528f072ae7995edccac1ad5a26e75
HiagoP <hiagoperonip@gmail.com>
2022-09-13 16:02:56 -0300

    Requisito 10 pronto :rocket:

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ItensCarrinho from '../components/ItensCarrinho';
 
 class Cart extends React.Component {
   state = {
     data: [],
-    disabled: true,
+    // disabled: false,
+    quantidade: 1,
   };
 
   componentDidMount() {
@@ -15,56 +17,39 @@ class Cart extends React.Component {
     if (data) this.setState({ data });
   };
 
-  aumentarProduto = ({ target }) => {
-    const soma = Number(target.parentNode.previousSibling.innerText) + 1;
-    target.parentNode.previousSibling.innerText = soma;
-    this.setState({ disabled: false });
+  // aumentarProduto = ({ target }) => {
+  //   // const soma = Number(target.parentNode.previousSibling.innerText) + 1;
+  //   // target.parentNode.previousSibling.innerText = soma;
+  //   // this.setState({ quantidade: soma });
+  //   // this.setState({ disabled: false });
+  // };
+
+  aumentarProduto = () => {
+    this.setState((previousState) => ({ quantidade: previousState.quantidade + 1 }));
   };
 
   diminuirProduto = ({ target }) => {
     const sub = Number(target.parentNode.previousSibling.innerText) - 1;
-    const minNumber = 3;
+    const minNumber = 2;
     if (Number(target.parentNode.previousSibling.innerText) < minNumber) {
-      this.setState({ disabled: true });
+      return true;
     }
     (target.parentNode.previousSibling.innerText = sub);
   };
 
   render() {
-    const { data, disabled } = this.state;
+    const { data } = this.state;
     return (
       <div>
         {
           data.length > 0 ? data.map(({ id, price, thumbnail, title }) => (
-            <div key={ id }>
-              <img src={ thumbnail } alt={ title } />
-              <p data-testid="shopping-cart-product-name">{ title }</p>
-              <p data-testid="shopping-cart-product-quantity">1</p>
-              <span>
-                <button
-                  type="button"
-                  data-testid="product-increase-quantity"
-                  onClick={ this.aumentarProduto }
-                >
-                  +
-                </button>
-                <button
-                  type="button"
-                  data-testid="product-decrease-quantity"
-                  disabled={ disabled }
-                  onClick={ this.diminuirProduto }
-                >
-                  -
-                </button>
-              </span>
-              <br />
-              <button type="button" data-testid="remove-product">Remover</button>
-              <p>
-                {' '}
-                {price}
-                {' '}
-              </p>
-            </div>
+            <ItensCarrinho
+              key={ id }
+              id={ id }
+              thumbnail={ thumbnail }
+              title={ title }
+              price={ price }
+            />
           ))
             : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
         }
